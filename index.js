5c254bf42d554aa0fc32581e3f36fd21b0fd885e
Byakuya91 <nganti91@yahoo.com>
2022-04-04 16:39:20 -0400

    I imported promt synch and successfully tested it into index.js. But the node start is not working. I will need to address it.

--- index.js
+++ index.js
@@ -16,12 +16,21 @@ let { withdraw, deposit, getBalance } = require("./atm");
 // test the functions to see if they are detected.
 
 // console.log(validatePin);
-console.log(withdraw);
-console.log(deposit);
-console.log(getBalance);
+// console.log(withdraw);
+// console.log(deposit);
+// console.log(getBalance);
 
 //TODO: Utilize prompt-sync so we can get user input for various functions
 //* Refer to Intro to Node.js PowerPoint for prompt-sync installation instructions
+
+// testing prompt synch
+const input = require("prompt-sync")();
+
+// console.log("Fire or Ice?");
+
+// const elementChoice = input();
+
+// console.log(`${elementChoice} is what I pick!`);
 
 function accessATM() {
   //TODO: Prompt users for their pin
