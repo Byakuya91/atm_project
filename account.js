5cfbbae60bce9254515f3ddba4e10d9d239d7f37
Byakuya91 <nganti91@yahoo.com>
2022-04-04 16:02:02 -0400

    I intialized the pin the balance variables and imported them to the atm.js file. I also completed and successfully tested the atm.js functions.

---
+++ account.js
@@ -0,0 +1,16 @@
+"use strict";
+//! START HERE! Then atm.js, then index.js
+//! Remember to run npm init to create your Node application!
+
+//TODO: Create variables for account balance & account pin
+
+let personalPin = 2191;
+
+let personalBalance = 1000;
+
+//Export these variables
+
+module.exports = {
+  personalPin: personalPin,
+  personalBalance: personalBalance,
+};
---
+++ atm.js
@@ -0,0 +1,50 @@
+"use strict";
+//TODO: Import necessary values from account.js
+const { personalPin } = require("./account");
+let { personalBalance } = require("./account");
+
+// test imported variables.
+// console.log(personalPin);
+// console.log(personalBalance);
+
+function getBalance() {
+  //TODO: Return the customer's acct. balance
+  return personalBalance;
+}
+
+function withdraw(withdrawAmount) {
+  //TODO: withdraw amount from current acct. balance
+  // Log the current balance after withdrawal is made
+  return (personalBalance -= withdrawAmount);
+}
+
+function deposit(depositAmount) {
+  //TODO: deposit amount to current acct. balance
+  // Log the current balance after deposit is made
+  return (personalBalance += depositAmount);
+}
+
+function validatePin(enteredPin) {
+  //TODO: Check if entered pin matches account.js pin
+  //Allow access to ATM if matching
+  //Return value should be a Boolean (true or false)
+  //In English: if the PIN entered is correct, return True. Otherwise, return False.
+
+  return enteredPin === personalPin;
+}
+
+// test the functions
+
+// let testBalance = getBalance();
+// console.log(testBalance);
+
+// let testWithdrawl = withdraw(100);
+// console.log(testWithdrawl);
+
+// let testDesposit = deposit(100);
+// console.log(testDesposit);
+
+let testPin = validatePin(2191);
+console.log(testPin);
+
+//TODO: Export these functions
